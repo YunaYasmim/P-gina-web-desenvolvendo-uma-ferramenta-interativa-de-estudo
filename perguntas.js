1f39cc24ab06739e226c3e3fd969d32b91be1bcd
YunaYasmim <125297887+YunaYasmim@users.noreply.github.com>
2024-10-11 13:38:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+  'Programação',
+  'O que é Python?',
+  'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+  'Geografia',
+  'Qual a capital do Japão?',
+  'A capital do Japão é Tokyo'
+  )
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+  'Lingua japonêsa',
+  'Como se diz obrigado em Japonês?',
+  'Obrigado em japonês é ありがとう (ARIGATÔ)'
+)
